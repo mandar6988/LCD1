829f7be3694b0c3b616ae0a4f9b2d023c0950ec5
Mandar Kulkarni <52269075+mandar6988@users.noreply.github.com>
2019-07-25 22:56:30 +0530

    Update main1.c

--- main1.c
+++ main1.c
@@ -5,11 +5,11 @@
 int main()
 {
  IODIR0 &= 0xFFFFFFFF;
-	 LCD_INIT();
+	 LCD_INIT();// Initialse lcd function to operate in 4bit mode 
     delay(1000);
-	  LCD_PRINT("LCD in 4 bit");
-	  LCD_CMD(0xC8);
-	  LCD_PRINT("MODE");
+	  LCD_PRINT("LCD in 4 bit");// print data on 1st row
+	  LCD_CMD(0xC8);// Command to go to second line
+	  LCD_PRINT("MODE"); // print data on second row
 	
 	  delay(10000);
 	 
